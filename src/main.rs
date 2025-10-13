2baf94094f42e48fb0e119477ec8b4a9a680831a
yxnne <506524339@qq.com>
2025-10-13 22:06:29 +0800

    feat: clap 解析参数

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,36 @@
-/// 这是一个简单的命令行工具
-/// 它会打印出 "Hello, rcli!"
-fn main() {
-    let greeting_str: &str = "Hello, rcli!";
+use clap::Parser;
 
-    // println!("{}", greeting_str);
-    println!("{greeting_str}");
+/// cargo add clap --features derive (只使用这个feature，clap：https://docs.rs/clap/latest/clap/_derive/_tutorial/index.html)
+fn main() {
+    let opts = Opts::parse();
+    println!("{:?}", opts);
+}
+
+#[derive(Debug, Parser)]
+#[command(name = "rcli", about = "A simple command line tool", version, author)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: Subcommand,
+}
+
+#[derive(Debug, Parser)]
+enum Subcommand {
+    #[command(name = "csv", about = "Convert csv to json")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    #[arg(short, long, help = "Input csv file")]
+    input: String,
+
+    #[arg(short, long, help = "Output json file", default_value = "output.json")]
+    // 字面量转化为 String
+    output: String,
+
+    #[arg(long, help = "Has header", default_value_t = true)]
+    header: bool,
+
+    #[arg(short, long, help = "Delimiter", default_value_t = ',')]
+    delimiter: char,
 }
