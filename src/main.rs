99f12bba8c91348818d19075e2c08cf1490162e2
yxnne <506524339@qq.com>
2025-10-13 18:47:32 +0800

    feat: git commit 检查终于提交上去了

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 /// 这是一个简单的命令行工具
+/// 它会打印出 "Hello, rcli!"
 fn main() {
     let greeting_str: &str = "Hello, rcli!";
 
-    println!("{}", greeting_str);    
+    // println!("{}", greeting_str);
+    println!("{greeting_str}");
 }
